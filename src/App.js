b0a75b4f56e4e4349221c0ab77b90347af2de354
Huy Hieu <huyhieugckm@gmail.com>
2021-01-06 23:21:44 +0700

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import ReactPlayer from 'react-player';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return<>
+        <AppBar position='fixed'>
+            <Toolbar>
+                <Typography variant="h6" >Custom Video Player</Typography>
+            </Toolbar>
+        </AppBar>
+        <Toolbar />
+        <Container maxWidth="md">
+            <ReactPlayer url="https://www.youtube.com/watch?v=I7v7WROhDLA" 
+            />
+        </Container>
+    </>
 }
 
 export default App;
