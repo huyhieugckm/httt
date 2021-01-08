091b0caa38cecba292ce015ed84a840be25a6137
Huy Hieu <huyhieugckm@gmail.com>
2021-01-09 00:53:17 +0700

    Completed 2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import ReactPlayer from 'react-player';
-function App() {
-    return<>
-        <AppBar position='fixed'>
-            <Toolbar>
-                <Typography variant="h6" >Custom Video Player</Typography>
-            </Toolbar>
-        </AppBar>
-        <Toolbar />
-        <Container maxWidth="md">
-            <ReactPlayer url="https://www.youtube.com/watch?v=I7v7WROhDLA" 
-            />
-        </Container>
-    </>
-}
 
+class App extends Component{
+    constructor(props) {
+        super(props);
+        this.state = { value: '' };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+    render() {
+        return <>
+            <AppBar position='fixed'>
+                <Toolbar>
+                    <Typography variant="h6" >Custom Video Player</Typography>
+                </Toolbar>
+            </AppBar>
+            <Toolbar />
+            <Container maxWidth="md">
+                <form>
+                    <label>
+                        Link:
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                    </label>
+                </form>
+                <ReactPlayer url={this.state.value} onChange={this.handleChange}
+                />
+            </Container>
+        </>
+    }
+}
 export default App;
+
